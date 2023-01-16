250872d7d0c538b336a5192fd87624cfc4bcf23b
Ajay Johar <ajayjohar3136@gmail.com>
2023-01-16 18:28:07 -0500

    Add controller package

--- src/main/java/com/Payroll/controller/MyWebController.java
+++ src/main/java/com/Payroll/controller/MyWebController.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.Payroll.service.BusinessService;
+
 @Component
 public class MyWebController {
 	
@@ -16,23 +18,4 @@ public class MyWebController {
 		return businessService.calculateSum();
 	}
 
-}
-
-@Component
-class BusinessService{
-	
-	@Autowired
-	private DataService dataservice;
-	
-	public long calculateSum() {
-		List<Integer> data = dataservice.getData();
-		return data.stream().reduce(Integer::sum).get();
-	}
-}
-
-@Component
-class DataService{
-	public List<Integer> getData(){
-		return Arrays.asList(10, 20, 30, 40);
-	}
 }
